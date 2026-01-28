5553c2dfb4b281a8d624a8c009a7ac280d69ade3
banti-kumar9128 <vk9128697037@gmail.com>
2026-01-28 15:47:45 +0530

    Fix import path for app context (case/extension corrected)

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
-export const AppContext = createContext(null);
+import { useAppContext } from "../context/AppContext";
 
 export const AppContextProvider = ({ children }) => {
   const navigate = useNavigate();
